650770292f66354463a3b4615d70cdec19f9d471
Sami bentaiba <140959042+samibentaiba@users.noreply.github.com>
2025-03-23 16:47:48 +0100

    style: update SectionHero and GetStartButton components for improved responsiveness and visual consistency

--- src/components/sections/home/Hero/SectionHero.tsx
+++ src/components/sections/home/Hero/SectionHero.tsx
@@ -2,38 +2,27 @@ import GetStartButton from "./ui/GetStartButton";
 
 export default function SectionHero() {
   return (
-    <div
-      className="relative h-[512px] z-0  p-0 m-0  w-full flex items-center justify-center bg-cover bg-center"
-      style={{ backgroundImage: "url('/images/Herosection.png')" }}
-    >
+    <div className="relative md:h-[512px] h-[575px] z-0 p-0 m-0 w-full flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: "url('/images/Herosection.png')" }}>
       {/* Black overlay with 10% opacity */}
       <div className="absolute inset-0 bg-[#00061b] opacity-40"></div>
 
-      <div className="relative container p-0 m-0 w-[75%]  flex items-center text-white">
-        <div className=" w-[80%] flex flex-col  gap-[4.5rem] mb-5">
+      <div className="relative container p-0 m-0 md:w-[75%] w-[90%] flex items-center text-white">
+        <div className=" w-[80%] flex flex-col gap-[2rem] md:gap-[4.5rem] mb-5">
           <h1
-            className="text-6xl p-0 m-0  font-semibold"
+            className="text-6xl p-0 m-0 text-[2.3rem] md:text-[3.30rem] w-[17rem] md:w-auto font-semibold"
             style={{
-              fontSize: "3.30rem", // Equivalent to Tailwind's text-6xl
-              padding: 0,
-              margin: 0,
-              fontWeight: 600, // Equivalent to font-semibold
-            }}
-          >
+              fontWeight: 600 // Equivalent to font-semibold
+            }}>
             La seule offre solaire <br /> avec garantie d'économies incluse
           </h1>
           <div>
-            <GetStartButton
-              text="Prendre RDV avec un expert solaire"
-              href="demande-devis-panneau-solaire"
-            />
+            <GetStartButton text="Prendre RDV avec un expert solaire" href="demande-devis-panneau-solaire" />
             <p
               style={{
                 fontSize: "14px",
                 paddingLeft: "34px",
-                boxSizing: "border-box",
-              }}
-            >
+                boxSizing: "border-box"
+              }}>
               Gratuit et sans engagement
             </p>
           </div>
--- src/components/sections/home/Hero/ui/GetStartButton.tsx
+++ src/components/sections/home/Hero/ui/GetStartButton.tsx
@@ -8,46 +8,27 @@ interface GetStartButtonProps {
   href: string;
 }
 
-export default function GetStartButton(
-  { text, hoveredText, href }: GetStartButtonProps,
-): ReactElement {
+export default function GetStartButton({ text, hoveredText, href }: GetStartButtonProps): ReactElement {
   return (
-    <div>
-      <Link
-        href={href}
-        className="
-    group relative inline-flex gap-2 items-center justify-center 
-     bg-[#fbac18]/75 text-white font-medium rounded-full 
-    border-[3px] border-[#ffffff]/75 hover:bg-[#fbac18]/100 hover:border-[#ffffff]/100 
+    <Link
+      href={href}
+      className="
+    group  inline-flex gap-2 items-center justify-center 
+     bg-[#fbac18]/75 text-white w-[90vw] rounded-full  md:w-auto
+    border-[3px] font-[650] border-[#ffffff]/75 hover:bg-[#fbac18]/100 hover:border-[#ffffff]/100 
     overflow-hidden
   "
-        style={{
-          padding: "13px 40px",
-          fontSize: "14px",
-          fontWeight: 650,
-        }}
-      >
-        <FaAngleDoubleRight
-          className="text-white text-2xl"
-          size={18}
-        />
+      style={{
+        padding: "13px 40px",
+        fontSize: "14px"
+      }}>
+      <FaAngleDoubleRight className="text-white md:text-2xl text-3xl " size={18} />
 
-        {/* First text, disappears on hover */}
-        <span
-          className="transition-all opacity-100 font-medium duration-500 group-hover:translate-y-[-20px] group-hover:opacity-1"
-          style={{ fontSize: "20px", fontWeight: 500 }}
-        >
-          {text}
-        </span>
+      {/* First text, disappears on hover */}
+      <span className="transition-all md:font-[650] font-[600] md:text-[20px] text-[18px]  opacity-100  duration-500 group-hover:translate-y-[-20px] group-hover:opacity-1">{text}</span>
 
-        {/* Second text, appears after hover effect */}
-        <span
-          className="absolute transition-all duration-500 opacity-1 group-hover:opacity-100 group-hover:translate-y-0 translate-x-[16px]"
-          style={{ fontSize: "20px", fontWeight: 500 }}
-        >
-          {hoveredText || text}
-        </span>
-      </Link>
-    </div>
+      {/* Second text, appears after hover effect */}
+      <span className="absolute transition-all md:font-[650] font-[600] md:text-[20px] text-[18px] duration-500 opacity-1 group-hover:opacity-100 group-hover:translate-y-0 translate-x-[16px]">{hoveredText || text}</span>
+    </Link>
   );
 }
