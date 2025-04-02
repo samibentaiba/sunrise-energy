095abd6a93ff11011b9b94e48864cb528529f73e
Sami bentaiba <140959042+samibentaiba@users.noreply.github.com>
2025-04-02 10:26:28 +0100

    style: add Pricing, CompanyInfo, and Trust components; refactor imports and remove obsolete files; implement FeatureList component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import Hero from "@/app/_components/Hero";
-import Features from "@/app/_components/Features";
-import Trust from "@/app/_components/Trust";
-import CTA from "@/app/_components/CTA";
-import Pricing from "@/app/_components/Pricing";
-import AdditionalGuarantees from "@/app/_components/AdditionalGuarantees";
-import Blog from "@/app/_components/Blog";
-import AboutSunVolt from "@/app/_components/CompanyInfo/AboutSunVolt";
-import Numbers from "@/app/_components/CompanyInfo/Numbers";
+import Hero from "@/components/pages/home/Hero";
+import Features from "@/components/pages/home/Features";
+import Trust from "@/components/pages/home/Trust";
+import CTA from "@/components/pages/home/CTA";
+import Pricing from "@/components/pages/home/Pricing";
+import AdditionalGuarantees from "@/components/pages/home/AdditionalGuarantees";
+import Blog from "@/components/pages/home/Blog";
+import AboutSunVolt from "@/components/pages/home/CompanyInfo/AboutSunVolt";
+import Numbers from "@/components/pages/home/CompanyInfo/Numbers";
 export default function Home() {
   return (
     <div className="min-h-screen overflow-clip w-[100%] flex flex-col">
--- src/app/panneux-solaires/page.tsx
+++ src/app/panneux-solaires/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import SectionHero from "@/app/panneux-solaires/_components/Hero";
-import SectionPricing from "@/app/panneux-solaires/_components/Pricing";
-import SectionAdditionalGuarantees from "@/app/panneux-solaires/_components/AdditionalGuarantees";
-import SectionBlog from "@/app/panneux-solaires/_components/Blog";
-import SectionSmartSolarInstallation from "@/app/panneux-solaires/_components/SmartSolarInstallation";
-import SectionSolarProtection from "@/app/panneux-solaires/_components/SolarProtection";
+import SectionHero from "@/components/pages/panneux-solaires/Hero";
+import SectionPricing from "@/components/pages/panneux-solaires/Pricing";
+import SectionAdditionalGuarantees from "@/components/pages/panneux-solaires/AdditionalGuarantees";
+import SectionBlog from "@/components/pages/panneux-solaires/Blog";
+import SectionSmartSolarInstallation from "@/components/pages/panneux-solaires/SmartSolarInstallation";
+import SectionSolarProtection from "@/components/pages/panneux-solaires/SolarProtection";
 export default function Home() {
   return (
     <div className="min-h-screen overflow-clip w-[100%] flex flex-col">
