702bc18248d4c1f295dfa15b8e75d460b5922d8d
Sami bentaiba <140959042+samibentaiba@users.noreply.github.com>
2025-04-05 11:58:17 +0100

    style: remove obsolete Garanties components and related imports

--- src/app/garanties/page.tsx
+++ src/app/garanties/page.tsx
@@ -3,15 +3,11 @@
 import Hero from "@/components/pages/garanties/Hero";
 import Guarantees from "@/components/pages/garanties/Guarantees";
 import Blog from "@/components/pages/garanties/Blog";
-import UPS from "@/components/pages/garanties/UPS";
-
 import Proches from "@/components/pages/garanties/Proches";
 import Reviews from "@/components/pages/garanties/Reviews";
 import FAQ from "@/components/pages/garanties/FAQ";
 import About from "@/components/pages/garanties/About";
 import Steps from "@/components/pages/garanties/Steps";
-import Technology from "@/components/pages/garanties/Technology";
-import Companies from "@/components/pages/garanties/Companies";
 import Team from "@/components/pages/garanties/Team";
 import ShowCase from "@/components/pages/garanties/ShowCase"
 export default function PanneuxSolaires() {
